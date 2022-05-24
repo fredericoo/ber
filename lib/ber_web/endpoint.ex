39b26d231c6a51f5bfb19c212866f9341b58b4dc
Bernardo Amorim <contato@bamorim.com>
2022-05-24 11:41:22 +0100

    fix: favicons

--- lib/ber_web/endpoint.ex
+++ lib/ber_web/endpoint.ex
@@ -20,7 +20,7 @@ defmodule BerWeb.Endpoint do
     at: "/",
     from: :ber,
     gzip: false,
-    only: ~w(assets fonts images favicon.ico robots.txt)
+    only: ~w(assets fonts images favicon.ico robots.txt site.webmanifest)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
